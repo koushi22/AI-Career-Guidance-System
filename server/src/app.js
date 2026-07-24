3ca0b64b182a5ad3e39d0dc86eaa9a5258a912e7
Koushika V <koushika489@gmail.com>
2026-07-24 09:24:49 +0530

    cors ku peru mathi crct ana link kuduthrukom ngaa

--- server/src/app.js
+++ server/src/app.js
@@ -15,6 +15,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
+  'https://ai-career-guidance-system-client.vercel.app/',
   process.env.CLIENT_URL
 ].filter(Boolean);
 
